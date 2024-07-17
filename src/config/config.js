17217472d878d949673e24dc3a8d7a6d88e69a23
fresh-finest <bb@brecx.com>
2024-07-17 14:39:31 +0600

    create config credentials

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+// config.js
+module.exports = {
+    clientId: 'amzn1.application-oa2-client.1e51478b36df46a1afa2ef7a1ae2f1f7',
+    clientSecret: 'YOUR_CLIENT_SECRET',
+    redirectUri: 'YOUR_REDIRECT_URI',
+    scopes: 'advertising::audiences advertising::campaign_management advertising::test:create_account',
+    authEndpoint: 'https://www.amazon.com/ap/oa',
+    tokenEndpoint: 'https://api.amazon.com/auth/o2/token'
+  };
+  
